b65c05f1057841efc69aec3ae4dad820e205ad9f
wlg1 <35532570+wlg1@users.noreply.github.com>
2023-01-16 13:52:32 -0500

    nose unit conv

--- video 1/3.2.py
+++ video 1/3.2.py
@@ -68,94 +68,99 @@ class scene_3_2(Scene):
         
         box = Rectangle(color=WHITE, height=5.7, width=6, stroke_width=1, fill_color=BLACK).shift(UP*0.33)
         box.z_index = 0
-                
-        ####################
-        # cat_person_zzz = VGroup(face_outline.copy(), left_eye_zzz.copy(), right_eye_zzz.copy(), left_ear.copy(), right_ear.copy(), nose.copy(), whiskers.copy(), mouth_smile.copy(), zzz.copy(), box.copy()).scale(0.3)
-          
-        # nap_space = NumberLine(
-            # x_range=[-1, 4, 1],
-            # color=BLUE,
-            # include_numbers=True,
-            # label_direction=UP,
-        # ).scale(2)
-        
-        # cat_person_zzz.move_to(nap_space.number_to_point(1))
-        
-        # self.add(nap_space, cat_person_zzz)
-                
-        # self.wait(3)
-        
-        # napSpaceName = Text('Nap Space', font_size=40).shift(DOWN*2.5)
-        # self.play(Write(napSpaceName))
-        # self.wait(1)
-        
-        ###################
-        # '''Each positive unit measures how big their smile is.'''
-                
-        # mouth_smile_2 = mouth_smile.copy().scale(1.5)
-        # cat_person_zzz_2 = VGroup(face_outline.copy(), left_eye_zzz.copy(), right_eye_zzz.copy(), left_ear.copy(), right_ear.copy(), nose.copy(), whiskers.copy(), mouth_smile_2.copy(), zzz.copy(), box.copy()).scale(0.3) 
-        # cat_person_zzz_2.move_to(nap_space.number_to_point(2))
-                
-        # self.play(Transform(cat_person_zzz, cat_person_zzz_2))
-        
-        # self.wait(1)
-        
-        # mouth_smile_3 = mouth_smile.copy().scale(2)
-        # cat_person_zzz_3 = VGroup(face_outline.copy(), left_eye_zzz.copy(), right_eye_zzz.copy(), left_ear.copy(), right_ear.copy(), nose.copy(), whiskers.copy(), mouth_smile_3.copy(), zzz.copy(), box.copy()).scale(0.3) 
-        # cat_person_zzz_3.move_to(nap_space.number_to_point(3))
-                
-        # self.play(Transform(cat_person_zzz, cat_person_zzz_3))
-        
-        # self.wait(1)
-        
-        ####################
-        '''We notice a pattern here that's somewhat like unit conversion. For instance, for every 1 meter, there are 3.28 feet.''' 
-        
-        text_1 = Text("1").shift(LEFT*2+UP*1.5)                
-        meter = Text("Meter").shift(LEFT*4+UP*1.5)
-        self.play(FadeIn(text_1, meter))
-        
-        into = Arrow([0,0,0],[1,0,0]).scale(4).shift(LEFT*0.5+UP*1.5)
-        self.play(FadeIn(into))
-                
-        text_2 = Text("3.28").shift(RIGHT*2+UP*1.5)
-        feet = Text("Feet").shift(RIGHT*4+UP*1.5)
-        self.play(FadeIn(text_2, feet))
-                
-        self.wait(1)
-        
-        '''So if an person is 2 meters tall, they're also 6.56 feet tall.'''
-        
-        text_3 = Text("2").shift(LEFT*2+DOWN*1.5)
-        text_4 = Text("6.56").shift(RIGHT*2+DOWN*1.5)
-                                  
-        meter_2 = Text("Meter").shift(LEFT*4+DOWN*1.5)
-        self.play(FadeIn(text_3, meter_2))
-        
-        into_2 = Arrow([0,0,0],[1,0,0]).scale(4).shift(LEFT*0.5+DOWN*1.5)
-        self.play(FadeIn(into_2))
-        
-        feet_2 = Text("Feet").shift(RIGHT*4+DOWN*1.5)
-        self.play(FadeIn(text_4, feet_2))
                         
-        self.wait(2)
-        
-        ####################
+        #####################
+        #'''We notice a pattern here that's somewhat like unit conversion. For instance, for every 1 meter, there are 3.28 feet.''' 
+        #
+        #text_1 = Text("1").shift(LEFT*2+UP*1.5)                
+        #meter = Text("Meter").shift(LEFT*4+UP*1.5)
+        #self.play(FadeIn(text_1, meter))
+        #
+        #into = Arrow([0,0,0],[1,0,0]).scale(4).shift(LEFT*0.5+UP*1.5)
+        #self.play(FadeIn(into))
+        #        
+        #text_2 = Text("3.28").shift(RIGHT*2+UP*1.5)
+        #feet = Text("Feet").shift(RIGHT*4+UP*1.5)
+        #self.play(FadeIn(text_2, feet))
+        #        
+        #self.wait(1)
+        #
+        #'''So if an person is 2 meters tall, they're also 6.56 feet tall.'''
+        #
+        #text_3 = Text("2").shift(LEFT*2+DOWN*1.5)
+        #text_4 = Text("6.56").shift(RIGHT*2+DOWN*1.5)
+        #                          
+        #meter_2 = Text("Meter").shift(LEFT*4+DOWN*1.5)
+        #self.play(FadeIn(text_3, meter_2))
+        #
+        #into_2 = Arrow([0,0,0],[1,0,0]).scale(4).shift(LEFT*0.5+DOWN*1.5)
+        #self.play(FadeIn(into_2))
+        #
+        #feet_2 = Text("Feet").shift(RIGHT*4+DOWN*1.5)
+        #self.play(FadeIn(text_4, feet_2))
+        #                
+        #self.wait(2)
+        #
+        #####################
         '''So let's convert nose units into nap units. Turning this into an equation, we have 2 * (nose) = 2 nap.'''
         
-        self.play(*[FadeOut(mob)for mob in self.mobjects])
+        # self.play(*[FadeOut(mob)for mob in self.mobjects])
         
         eqn_1 = Tex("2 * Nose = 2 Nap", font_size=53)
-        self.play(FadeIn(eqn_1))
+        # self.play(FadeIn(eqn_1))
+        # self.add(eqn_1)
         
         self.wait(2)
         
         ####################
-        '''And visually speaking, this means that for every one unit of nose tip, there are two units of Nap Smile. For every two units of nose tip, there are four units of Nap Smile.'''
+        '''And visually speaking, this means that for every one unit of nose tip, there are two units of Nap Smile.'''
         
-        #nose number line at top
+        nose_space = NumberLine(
+            x_range=[0, 4, 1],
+            include_numbers=True,
+            label_direction=UP,
+        ).scale(2).shift(UP*2)
         
+        nap_space = NumberLine(
+            x_range=[0, 4, 1],
+            include_numbers=True,
+            label_direction=UP,
+        ).scale(2).shift(DOWN*2)
         
+        self.add(nose_space, nap_space)
+        
+        eqn_1.generate_target()
+        eqn_1.target.shift(RIGHT*2)
+        
+        # self.play(MoveToTarget(eqn_1), GrowFromPoint(nose_space, eqn_1), GrowFromPoint(nap_space, eqn_1))
+        
+        # nose_group_1 = VGroup(face_outline.copy(), nose.copy(), box.copy()).scale(0.3) 
+        nose_group_1 = VGroup(face_outline.copy(), nose.copy()).scale(0.3)  
+        nose_group_1.move_to(nose_space.number_to_point(1))
+        self.add(nose_group_1)
+        
+        weight_2 = Tex("2 * ", font_size=53).move_to(nose_space.number_to_point(1)).shift(DOWN*1.5)
+        self.add(weight_2)
+        nose_unit_1 = Line(nose_space.number_to_point(0), nose_space.number_to_point(1), stroke_width=10, color=RED)
+        self.add(nose_unit_1)
+        
+        nose_unit_2 = Line(nose_space.number_to_point(0), nose_space.number_to_point(2), stroke_width=10, color=RED).shift(DOWN*1.5)
+        
+        # nose_unit_1_DM = VGroup(nose_unit_1, nose_group_1)
+        # nose_unit_1_DM_copy = nose_unit_1_DM.copy()
+        
+        # self.play(Transform(nose_unit_1_DM_copy, nose_unit_2), Transform(weight_2, nose_unit_2))
+        
+        nose_unit_1_copy = nose_unit_1.copy()
+        nose_group_1_copy = nose_group_1.copy()
+        
+        nose_group_2 = nose_group_1.copy().move_to(nose_space.number_to_point(2)).shift(DOWN*1.5)
+        
+        self.play(Transform(nose_unit_1_copy, nose_unit_2), Transform(nose_group_1_copy, nose_group_2), Transform(weight_2, nose_unit_2))
+        
+        self.wait(1)
+        
+        ''' For every two units of nose tip, there are four units of Nap Smile.'''
         
         
         
