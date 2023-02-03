918ccd566b0ce48139dfa068721db5b6e895c1b4
wlg1 <35532570+wlg1@users.noreply.github.com>
2023-02-03 03:02:48 -0500

    2/4.5625 + 0.75/4.5625 is sent to 1. That's 0.60274, which is where the x-axis is sent to '1' in nap space.

--- video 1/6.3_test.py
+++ video 1/6.3_test.py
@@ -38,7 +38,7 @@ class scene_5_1(Scene):
                 # not true for other values b/c they are [2, 0.75] * [x,y], which doesn't square x and y
         # sqrt( (4/4.5625)^2 + (1.5/4.5625)^2 ) = 0.93632 = 0.46816 * 2
 
-        # disregard below; actually you can't roatte nap space directly to this vector [2, 0.75]. the ortho proj is NOT the result of a rotation! It is an orth proj.
+        # actually you can't rotate nap space directly to this vector [2, 0.75]. the ortho proj is NOT the result of a rotation! 
 
         # if you place nap space on x-axis (purp) and rotate it, the 1 is sent to 
         # the 2*0.46816 "length" mark IN TERMS OF INPUT SPACE, but it will be '2' on nap space b/c nap space considers '0.46816' to be '1'. This is like how feet and meters differ, or meters and kilometers. 1 kilometer considers '1000' to be '1'.  1 meter considers '0.001' to be '1'. 
@@ -46,6 +46,10 @@ class scene_5_1(Scene):
                 # may transf it to colored N or dot b/c too small
             # model != data pts, and since vectors built on data pts, != vectors either
 
+        # so actually 0.46816 IS sigf. 2/4.5625 = 0.43835 and 0.75/4.5625 are NOT the orth proj down; the orth proj down will hit '0.46816' on the x-axis, just like how 2*0.46816 will hit '1' after orth proj. actually no that's impossible; pythagorean theorem will make the 'b' be 0 if that's the case.
+
+        # 2/4.5625 is NOT sent to 1. It is actually 2/4.5625 + 0.75/4.5625 is sent to 1. That's 0.60274, which is where the x-axis is sent to '1' in nap space. 
+        # if you make a square following 2/4.5625, using 0.75/4.5625 as side length, the diagonal is the orthogonal projection down to x-axis, so it will land at 2/4.5625 + 0.75/4.5625 = 0.60274.
 
         ###############
         # get coordinate space, nose ear spaces, and nap_space_IS
