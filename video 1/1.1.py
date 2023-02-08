29b5b4389419cc72ddb6ac7eb2a692fa6951366c
wlg1 <35532570+wlg1@users.noreply.github.com>
2023-02-08 11:57:17 -0500

    sleeping human anim, 1.1.py

--- video 1/1.1.py
+++ video 1/1.1.py
@@ -17,8 +17,8 @@ class scene_1_1(Scene):
                 
         face_outline = Circle(radius=1.75, color=WHITE, fill_color=BLACK, fill_opacity=1).move_to([0, 0, 0])
         
-        left_eye = Dot(color=WHITE).move_to([-1, 0.5, 0])
-        right_eye = Dot(color=WHITE).move_to([1, 0.5, 0])
+        # left_eye = Dot(color=WHITE).move_to([-1, 0.5, 0])
+        # right_eye = Dot(color=WHITE).move_to([1, 0.5, 0])
         
         nose_line_1 = Line([-0.5, 0, 0], [0.5, 0, 0])
         nose_line_2 = Line([-0.5, 0, 0], [0, nose_tip, 0])
@@ -39,8 +39,8 @@ class scene_1_1(Scene):
         right_ear.z_index=1
                
         face_outline.z_index=3
-        left_eye.z_index=3
-        right_eye.z_index=3
+        # left_eye.z_index=3
+        # right_eye.z_index=3
                 
         for nl in nose:
             nl.z_index=3
@@ -48,11 +48,31 @@ class scene_1_1(Scene):
         for whisk in whiskers:
             whisk.z_index=3
             
-        face_group = VGroup(face_outline.copy(), left_eye.copy(), right_eye.copy(), left_ear.copy(), right_ear.copy(), nose.copy(), whiskers.copy()).scale(0.3).move_to(np.array([-3, 1, 0]))   
+        # face_group = VGroup(face_outline.copy(), left_eye.copy(), right_eye.copy(), left_ear.copy(), right_ear.copy(), nose.copy(), whiskers.copy()).scale(0.3).move_to(np.array([-3, 1, 0]))   
         
-        ##################################################
-        human_face = face_group[0].copy()  
-        human_face.z_index= 2
+        ##################################################       
+        
+        left_brow = ArcBetweenPoints([-0.3, -0.75,0], [0.3, -0.75,0]).rotate(180*DEGREES).move_to([-0.6, 0.2, 0])
+        right_brow = ArcBetweenPoints([-0.3, -0.75,0], [0.3, -0.75,0]).rotate(180*DEGREES).move_to([0.6, 0.2, 0])
+        left_brow.z_index=3
+        left_brow.stroke_width = 2
+        right_brow.z_index=3
+        right_brow.stroke_width = 2
+
+        left_eye_zzz = ArcBetweenPoints([-0.5, -0.9,0], [0.5, -0.9,0]).rotate(180*DEGREES).move_to([-0.7, -0.2, 0])
+        right_eye_zzz = ArcBetweenPoints([-0.5, -0.9,0], [0.5, -0.9,0]).rotate(180*DEGREES).move_to([0.7, -0.2, 0])
+        # left_eye = Line([-0.3, -0.75,0], [0.3, -0.75,0]).rotate(180*DEGREES).move_to([-0.6, -0.2, 0])
+        # right_eye = Line([-0.3, -0.75,0], [0.3, -0.75,0]).rotate(180*DEGREES).move_to([0.6, -0.2, 0])
+        left_eye_zzz.z_index=3
+        right_eye_zzz.z_index=3
+        left_eye_zzz.stroke_width = 2
+        right_eye_zzz.stroke_width = 2
+
+        left_eye = Dot(color=WHITE).move_to([-0.6, -0.2, 0]).scale(2)
+        right_eye = Dot(color=WHITE).move_to([0.6, -0.2, 0]).scale(2)
+        left_eye.z_index=3
+        right_eye.z_index=3
+        human_face = VGroup(face_outline.copy(), left_eye.copy(), right_eye.copy(), left_brow.copy(), right_brow.copy()).scale(0.3).move_to(np.array([-3, 1, 0]))   
         neck = Line([-3, 0, 0], [-3, 0.4, 0])
         
         top_line = Line([-3.75, 0, 0], [-2.25, 0, 0])
@@ -67,8 +87,13 @@ class scene_1_1(Scene):
         right_leg = Line([-3, -1.75, 0], [-2.5, -3.25, 0])
         
         tail = ArcBetweenPoints([-3, -1.75, 0], [-1.2, -1.2,0])
-        # human = VGroup(human_face, neck, body, left_arm, right_arm, left_leg, right_leg)
-        human_top = VGroup(human_face, neck, body, left_arm, right_arm)
+        
+        human_top = VGroup(human_face, neck.copy(), body.copy(), left_arm.copy(), right_arm.copy())
+
+        ###
+        human_face_2 = VGroup(face_outline.copy(), left_eye_zzz.copy(), right_eye_zzz.copy()).scale(0.3).move_to(np.array([-3, 1, 0]))   
+
+        human_top_2 = VGroup(human_face_2, neck.copy(), body.copy(), left_arm.copy(), right_arm.copy()).scale(0.75).move_to([3, -1.5, 0]).shift(UP)
 
         ##################################################
 
@@ -82,9 +107,21 @@ class scene_1_1(Scene):
         book = VGroup(book_top_left, book_top_right, book_left, book_bot_left, book_bot_right, book_right, book_mid)
         
         ##################################################
-        cat_face = face_group.copy().move_to([1, -1.5, 0]).scale(0.65)
-        self.add(cat_face) 
+        # cat_face = face_group.copy().move_to([1, -1.5, 0]).scale(0.65)
+        # self.add(cat_face) 
         
-        pure_human = human_top.copy().scale(0.75).move_to([3, -1.5, 0]).shift(UP)
+        human_top.scale(0.75).move_to([3, -1.5, 0]).shift(UP)
         book.scale(0.35).move_to([3, -1.5, 0]).shift(DOWN*0.5)
-        self.add(pure_human, book)
+        self.add(human_top, book)
+
+        self.wait(2)
+
+        self.play(Transform(human_face[1], human_face_2[1]), Transform(human_face[2], human_face_2[2]), FadeOut(human_face[3], human_face[4]))
+
+        self.wait(2)
+
+        # zzz = Text("...zZz...", font_size = 40, color=WHITE).scale(0.75).move_to([3, -1.5, 0]).shift(UP)
+        
+        # self.play(Write(zzz))
+        # self.wait(1)
+        # self.play(Unwrite(zzz))
